900a00d5dc863144de4bd38a8e77f4c7e44c4cc8
Roman Lemeshov <stack.overflower01@gmail.com>
2025-01-15 23:46:50 +0200

    custom exceptions

---
+++ backend/Diary/Diary.BLL/Exceptions/InvalidTokenException.cs
@@ -0,0 +1,7 @@
+﻿namespace Diary.BLL.Exceptions
+{
+    public class InvalidTokenException : Exception
+    {
+        public InvalidTokenException() : base("Token is invalid") { }
+    }
+}
---
+++ backend/Diary/Diary.WebAPI/Extentions/ExceptionFilterExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Diary.BLL.Exceptions;
+using Diary.WebAPI.Enums;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Net;
+
+namespace Diary.WebAPI.Extentions
+{
+    public static class ExceptionFilterExtensions
+    {
+        public static (HttpStatusCode statusCode, ErrorCode errorCode) ParseException(this Exception exception)
+        {
+            return exception switch
+            {
+                BadRequestException _ =>(HttpStatusCode.BadRequest,ErrorCode.BadRequest),
+                NotFoundException _ => (HttpStatusCode.NotFound, ErrorCode.NotFound),
+                InvalidCredentialsException _ => (HttpStatusCode.Unauthorized, ErrorCode.InvalidUsernameOrPassword),
+                InvalidTokenException _ => (HttpStatusCode.Unauthorized, ErrorCode.InvalidToken),
+                _ => (HttpStatusCode.InternalServerError, ErrorCode.General),
+            };
+        }
+    }
+}
